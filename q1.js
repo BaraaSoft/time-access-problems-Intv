5be138f75ea68ab8935108ec6791a370083ca329
baraaabuzeid <baraa@aleph-labs.com>
2021-12-20 19:23:20 +0200

    - change q1 solution accounting for slips with negative number.

--- q1.js
+++ q1.js
@@ -17,33 +17,57 @@ If no combo of the slips can add to the target value, return -1.
    * @return {number} 
 */
 var leastSlips = function (slips, target) {
-    // fill in your solution here. 
-   
-    let resultArr = combo(slips, target)
-    if(resultArr){
-        //console.log(resultArr)
-        return resultArr.length
+    let n = slips.length;
+
+    let hasPositives = false;
+    for (let j = 0; j < n; j++)
+        if (slips[j] > 0) {
+            hasPositives = true;
+            break;
+        }
+    if (!hasPositives) return -1;
+
+    let min = 0;
+
+    const P = []; // prefix sums
+    P[0] = 0;
+    let positiveSum = 0;
+    for (let j = 0; j < n; j++) {
+        if (slips[j] >= target) return 1;
+        P[j + 1] = P[j] + slips[j];
+
+        if (slips[j] <= 0) continue;
+        if (slips[j] > 0) positiveSum += slips[j];
+        if (positiveSum < target) continue;
+
+        for (let i = 1; i <= (min ? min - 1 : j + 1); i++)
+            if (P[j + 1] - P[j + 1 - i] >= target) {
+                min = i;
+                break;
+            }
     }
-    return -1
+
+    return min || -1;
 };
 
-const combo = (arr,target)=>{
-    if(target == 0) return []
-    if(target < 0) return  null;
 
-    let result = null;
+// const combo = (arr,target)=>{
+//     if(target == 0) return []
+//     if(target < 0) return  null;
 
-    for(let num of arr){
-        let remainder = target - num;
-        let curr = combo(arr,remainder)
+//     let result = null;
 
-        if(curr){
-            curr = [...curr,num]
-            result = (result == null || result.length > curr.length )? curr : result
-        }
-    }
-    return result;
-}
+//     for(let num of arr){
+//         let remainder = target - num;
+//         let curr = combo(arr,remainder)
+
+//         if(curr){
+//             curr = [...curr,num]
+//             result = (result == null || result.length > curr.length )? curr : result
+//         }
+//     }
+//     return result;
+// }
 
 
 
